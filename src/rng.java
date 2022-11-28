7fd0d7029d7667951a4d8487c2c8ab2f64ae265d
BT_3W2_06 <simonh8@nycstudents.net>
2022-11-28 09:29:33 -0500

    1

--- src/rng.java
+++ src/rng.java
@@ -1,31 +1,43 @@
 public class rng {
     private int counter;
-    private int times;
     private String target;
     private String[] targetList = {"A", "B", "C", "D", "E"};
+    private int bal;
 
     public rng(){ //default
-        times = 1;
         target = targetList[((int)(Math.random()*5))];
+        bal = 100;
     }
 
-    public rng(int times){ //custom attempts
-        this.times = times;
+    public rng(String target) { //custom attempts
+        this.target = target;
+        bal = 100;
+    }
+
+    public rng(String item1, String item2,String item3, String item4, String item5){
+        targetList = targetList[item1, item2, item3, item4, item5];
     }
 
     public int randomNum(){ //generates number for outcome
-        int random = (int)(Math.random()*100);
+        int random = (int)(Math.random()*100+1);
         if (random == 1){
             if ((int)(Math.random()*2) == 1)
                 return 1;
+            else return 2;
         }else if (random <= 5){
-            return 2;
-        }else if (random <= 25){
             return 3;
-        }else return 4;
+        }else if (random <= 25){
+            return 4;
+        }else return 5;
     }
 
     public void outcome(){
-
+        bal--;
+        String obtained = targetList[randomNum()];
+        if (obtained.equals(target)){
+            System.out.println("You have obtained your target!");
+        }else{
+            System.out.println("You did not obtain your target...");
+        }
     }
 }
--- src/runner.java
+++ src/runner.java
@@ -5,6 +5,11 @@ public class runner {
         Scanner input = new Scanner(System.in);
         System.out.println("Do you want default settings? (y/n)");
         String defaultSet = input.nextLine();
+        for (int i = 0; i < 100; i++){
+            int random = (int)(Math.random()*100+1);
+            System.out.println(random);
+
+        }
 
 
     }
